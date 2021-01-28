0bb3b1f545f36babf6a2ed5ba5c66fd58375e18d
Nolan Nordlund <nolan@projectrebel.io>
2021-01-28 17:14:38 -0600

    Added and configured breeze

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,24 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: [
-    './storage/framework/views/*.php',
-    './resources/**/*.blade.php',
-    './resources/**/*.js',
-    './resources/**/*.vue',
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Montserrat', ...defaultTheme.fontFamily.sans]
-      }
+    purge: [
+        "./storage/framework/views/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    variants: {
+        extend: {
+            opacity: ["disabled"],
+        },
+    },
+    plugins: [require("@tailwindcss/forms")],
+};
